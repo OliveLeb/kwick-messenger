f2381413856149e3a72560ca6df01fd0cc8b8d35
OliveLeb <lebel.olivier@wanadoo.fr>
2021-01-06 03:11:01 +0100

    dataService

---
+++ src/services/dataService.js
@@ -0,0 +1,16 @@
+import http from './http.common';
+
+const getUsers = (token) => {
+    return http.get(`/user/logged/${token}`);
+};
+
+const getMessages = (token,timestamp) => {
+    return http.get(`/talk/list/${token}/${timestamp}`);
+};
+
+const postMessage = (token,id,message) => {
+    return http.get(`/say/${token}/${id}/${message}`);
+};
+
+const dataService = { getUsers, getMessages, postMessage };
+export default dataService;
